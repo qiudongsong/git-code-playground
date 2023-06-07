57e4dc2617fb004d59490a6d8e7d0335d914e379
Qiudong Song <qiudongsong@gmail.com>
2023-06-07 16:39:31 +0800

    first commit

---
+++ python learning practice.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def my_function(arg1, arg2):   # Defines a new function
+    return arg1 + arg2           # Function body (code to execute)
+
+
+# In[4]:
+
+
+my_function(5, 10)
+
+
+# In[ ]:
+
+def sum_3_items(x, y, z, print_args = False): 
+    if print_args:                        
+        print(x,y,z)
+    return x + y + z
+
+
+sum_3_items(5,10,20)        # By default the 
+
